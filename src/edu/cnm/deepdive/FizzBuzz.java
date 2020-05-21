8e95395bd22317e03782fa35e4bd472b13e5a1b1
Nick Bennett <nick@nickbenn.com>
2020-05-21 14:29:35 -0600

    Use status string for fizzbuzz.

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -4,17 +4,19 @@ public class FizzBuzz {
 
   public static void main(String[] args) {
     for (int counter = 1; counter <= 100; counter++) {
+      String status = "";
       boolean counterIsFizz = (counter % 3 == 0);
       boolean counterIsBuzz = (counter % 5 == 0);
-      if (counterIsFizz && counterIsBuzz) {
-        System.out.println("fizzbuzz");
-      } else if (counterIsFizz) {
-        System.out.println("fizz");
-      } else if (counterIsBuzz) {
-        System.out.println("buzz");
-      } else {
-        System.out.println(counter);
+      if (counterIsFizz) {
+        status += "fizz";
       }
+      if (counterIsBuzz) {
+        status += "buzz";
+      }
+      if (status.isEmpty()) {
+        status += counter;
+      }
+      System.out.println(status);
     }
   }
 
